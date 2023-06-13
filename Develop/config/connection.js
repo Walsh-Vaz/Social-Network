1fdb042867b491dc788b652d46dcf9c5457f0a1a
Walsh Vaz <walshivaz@yahoo.com>
2023-06-13 13:19:37 -0400

    updated the connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,8 +1,12 @@
 const {connect, connection} = require("mongoose");
 
-connect("mongodb://localhost/socialNetwork", {
+connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network", 
+
+{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}
+
+);
 
 module.exports = connection;
