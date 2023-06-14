ee77461d3ffff5d7345d56af2d54138d5b19eddb
Walsh Vaz <walshivaz@yahoo.com>
2023-06-14 16:41:26 -0400

    added the routers for users

---
+++ Develop/routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+const {getUsers, getUserById, createUser, updateUser, deleteUser} = require("../../controllers/userController");
+
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
